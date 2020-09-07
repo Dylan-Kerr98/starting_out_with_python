47139ce1c458973746c381c133fb2689afccabbc
dylankerr <dylan--kerr@outlook.com>
2020-09-07 19:16:42 +0100

    New file

---
+++ Chapter 1 - Input, Processing & Output/23 - formatting_scientific_notation.py
@@ -0,0 +1,4 @@
+# If you prefer to display floating-point numbers in scientific notation, you can use the letter e or E instead of f
+
+print(format(12345.6789, 'e'))
+print(format(12345.6789, '.2e'))
