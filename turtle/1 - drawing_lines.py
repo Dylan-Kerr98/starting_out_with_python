ab4dc1386e2b9b3488006138e741ec45e23a20e3
dylankerr <dylan--kerr@outlook.com>
2020-09-08 22:46:52 +0100

    New file

---
+++ turtle/1 - drawing_lines.py
@@ -0,0 +1,27 @@
+# Turtle graphics is an interesting and easy way to learn basic programming concepts
+# The Python turtle graphics system simulates a "turtle" that obeys commands to draw simple graphics
+
+# The first step in using Python's turtle graphics is the write the following statement:
+
+import turtle
+
+# This statement is necessary because the turtle graphics system is not built into the Python interpreter and instead
+# is stored in a file known as the turtle module
+
+# The import statement loads the turtle module into memory so the Python interpreter can use it
+
+# The Python turtle is initially positioned in the centre of a graphics window that serves as a canvas
+
+# You can enter the following command to display the turtle in its window:
+
+turtle.showturtle()
+
+# Notice the turtle is the shape of an arrow, this is to show the direction that the turtle is currently facing
+
+# The following command makes the turtle advance forward, this distance is given as an argument
+
+turtle.forward(300)
+
+# The following command makes the turtle advance forward, this distance is given as an argument
+
+turtle.done()    # This command means the graphic window will stay open as long as you require it
---
+++ turtle/2 - turning_turtle.py
@@ -0,0 +1,14 @@
+import turtle
+# When the turtle first appears, its default heading is 0 degrees (East)
+
+turtle.forward(50)
+
+# There are two commands that can be used to turn the direct of the turtle, angle degrees of turn is passed to
+# these as an argument
+
+turtle.left(90)    # Turns turtle left
+turtle.forward(200)
+turtle.right(45)    # Turns turtle right
+turtle.forward(200)
+
+turtle.done()
